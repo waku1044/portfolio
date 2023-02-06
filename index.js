66b4b3ba510928fe5f993b2c36b563f968f2ad6e
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-02-06 10:09:05 -0300

    Se agrego modal en habilidades con certificado

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const $menu =  document.querySelector('.menu p');
 const $items = document.querySelector('.items');
+const $dialog = document.querySelector('#dialog');
+const $certif = document.querySelector('#modalCer');
+const $titulo = document.querySelector('#titulo');
 
 function toogle(){
 	switch($menu.innerHTML){
@@ -22,9 +25,50 @@ function hiddenOption(e){
 	console.log(this)
 	$items.classList.add('hidden');
 	$menu.innerHTML = 'Menu';
-	
-	
-
 }
+
+function cerrar(){
+	$dialog.close();
+}
+
+function modal(event){
+		switch(event.id){
+		case 'html':
+			$titulo.innerHTML = '';
+			$certif.style.display ='block';
+			$certif.src = './imagenes/certificado_html.png';
+			$dialog.showModal();
+			break;
+		case 'css':
+			$titulo.innerHTML = '';
+			$certif.style.display ='block';
+			$certif.src = './imagenes/certificado_css.png';
+			$dialog.showModal();
+			break;
+		case 'js':
+			$titulo.innerHTML = '';
+			$certif.style.display ='block';
+			$certif.src = './imagenes/certificado_js.png';
+			$dialog.showModal();
+			break;
+		case 'jquery':
+			$titulo.innerHTML = '';
+			$certif.style.display ='block';
+			$certif.src = './imagenes/certificado_jquery.png';
+			$dialog.showModal();
+			break;
+		default:
+			console.log(event.id)
+			$titulo.innerHTML = 'Sin certificado por el momento.';
+			$certif.style.display = 'none';
+			$dialog.showModal();
+			break; 
+
+	}
+	
+	
+}
+
+
 $menu.onclick = toogle;
 options.onclick = hiddenOption;
